a91c8d341b03dfa0069801b2f9191306a1e38bd4
Semionn <mrSemionn@gmail.com>
2014-06-29 12:46:25 +0600

    Initial

---
+++ MainForm.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace TesterClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxThemes = new System.Windows.Forms.ListBox();
+            this.listBoxTests = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.buttoNext = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.listBoxAnswers = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTask = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTaskNum = new System.Windows.Forms.Label();
+            this.labelChooseTest = new System.Windows.Forms.Label();
+            this.buttonBeginTest = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxThemes
+            // 
+            this.listBoxThemes.FormattingEnabled = true;
+            this.listBoxThemes.Location = new System.Drawing.Point(3, 31);
+            this.listBoxThemes.Name = "listBoxThemes";
+            this.listBoxThemes.Size = new System.Drawing.Size(117, 264);
+            this.listBoxThemes.TabIndex = 0;
+            this.listBoxThemes.SelectedIndexChanged += new System.EventHandler(this.listBoxThemes_SelectedIndexChanged);
+            // 
+            // listBoxTests
+            // 
+            this.listBoxTests.FormattingEnabled = true;
+            this.listBoxTests.Location = new System.Drawing.Point(126, 31);
+            this.listBoxTests.Name = "listBoxTests";
+            this.listBoxTests.Size = new System.Drawing.Size(188, 264);
+            this.listBoxTests.TabIndex = 1;
+            this.listBoxTests.SelectedIndexChanged += new System.EventHandler(this.listBoxTests_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listBoxThemes);
+            this.panel1.Controls.Add(this.listBoxTests);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(322, 411);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Тесты";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Разделы";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.labelTaskNum);
+            this.panel2.Controls.Add(this.buttonEnd);
+            this.panel2.Controls.Add(this.buttoNext);
+            this.panel2.Controls.Add(this.buttonBack);
+            this.panel2.Controls.Add(this.listBoxAnswers);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.textBoxTask);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(322, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(533, 411);
+            this.panel2.TabIndex = 3;
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Location = new System.Drawing.Point(401, 363);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(120, 36);
+            this.buttonEnd.TabIndex = 5;
+            this.buttonEnd.Text = "Закончить";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.buttonEnd_Click);
+            // 
+            // buttoNext
+            // 
+            this.buttoNext.Location = new System.Drawing.Point(143, 363);
+            this.buttoNext.Name = "buttoNext";
+            this.buttoNext.Size = new System.Drawing.Size(121, 36);
+            this.buttoNext.TabIndex = 4;
+            this.buttoNext.Text = "Дальше";
+            this.buttoNext.UseVisualStyleBackColor = true;
+            this.buttoNext.Click += new System.EventHandler(this.buttoNext_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(6, 363);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(121, 36);
+            this.buttonBack.TabIndex = 3;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // listBoxAnswers
+            // 
+            this.listBoxAnswers.BackColor = System.Drawing.Color.PowderBlue;
+            this.listBoxAnswers.FormattingEnabled = true;
+            this.listBoxAnswers.Location = new System.Drawing.Point(6, 171);
+            this.listBoxAnswers.Name = "listBoxAnswers";
+            this.listBoxAnswers.Size = new System.Drawing.Size(515, 186);
+            this.listBoxAnswers.TabIndex = 2;
+            this.listBoxAnswers.SelectedIndexChanged += new System.EventHandler(this.listBoxAnswers_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Задание";
+            // 
+            // textBoxTask
+            // 
+            this.textBoxTask.Location = new System.Drawing.Point(6, 31);
+            this.textBoxTask.Multiline = true;
+            this.textBoxTask.Name = "textBoxTask";
+            this.textBoxTask.Size = new System.Drawing.Size(515, 134);
+            this.textBoxTask.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // labelTaskNum
+            // 
+            this.labelTaskNum.AutoSize = true;
+            this.labelTaskNum.Location = new System.Drawing.Point(192, 13);
+            this.labelTaskNum.Name = "labelTaskNum";
+            this.labelTaskNum.Size = new System.Drawing.Size(83, 13);
+            this.labelTaskNum.TabIndex = 6;
+            this.labelTaskNum.Text = "Вопрос 1 из 10";
+            // 
+            // labelChooseTest
+            // 
+            this.labelChooseTest.AutoSize = true;
+            this.labelChooseTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelChooseTest.Location = new System.Drawing.Point(517, 93);
+            this.labelChooseTest.Name = "labelChooseTest";
+            this.labelChooseTest.Size = new System.Drawing.Size(124, 20);
+            this.labelChooseTest.TabIndex = 4;
+            this.labelChooseTest.Text = "Выберите тест";
+            // 
+            // buttonBeginTest
+            // 
+            this.buttonBeginTest.Location = new System.Drawing.Point(521, 133);
+            this.buttonBeginTest.Name = "buttonBeginTest";
+            this.buttonBeginTest.Size = new System.Drawing.Size(111, 33);
+            this.buttonBeginTest.TabIndex = 5;
+            this.buttonBeginTest.Text = "Начать тест";
+            this.buttonBeginTest.UseVisualStyleBackColor = true;
+            this.buttonBeginTest.Visible = false;
+            this.buttonBeginTest.Click += new System.EventHandler(this.buttonBeginTest_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(855, 411);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.labelChooseTest);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonBeginTest);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxThemes;
+        private System.Windows.Forms.ListBox listBoxTests;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.Button buttoNext;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.ListBox listBoxAnswers;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTask;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTaskNum;
+        private System.Windows.Forms.Label labelChooseTest;
+        private System.Windows.Forms.Button buttonBeginTest;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TesterClient.ServiceReference1;
+
+namespace TesterClient
+{
+    public partial class MainForm : Form
+    {
+        EnterForm enterForm = null;
+        Service1Client client = null;
+        List<TestsTheme> tests = null;
+        TestPassage currTestPass = null;
+        User currUser = null;
+        bool init = true;
+        DateTime taskBeginTime = DateTime.Now;
+
+        public MainForm(EnterForm enterForm, User user)
+        {
+            InitializeComponent();
+            this.enterForm = enterForm;
+            client = enterForm.client;
+            currUser = user;
+            tests = client.GetAllTests();
+            Text = "Пользователь: " + user.Name;
+            listBoxThemes.DataSource = tests.Select(a => a.Name).ToList();
+            panel2.Hide();
+            init = false;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            enterForm.Close();
+        }
+
+        private void listBoxThemes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxThemes.SelectedIndex > -1)
+            {
+                listBoxTests.DataSource = tests[listBoxThemes.SelectedIndex].Tests.Select(a => a.Name).ToList();
+            }
+        }
+
+        private void listBoxTests_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!init && listBoxTests.SelectedIndex > -1)
+            {
+                buttonBeginTest.Visible = true;           
+            }
+            else
+            {
+                listBoxTests.SelectedIndex = -1;
+                buttonBeginTest.Visible = false;
+            }
+        }
+
+        private void ShowTask()
+        {
+            SetLabelTaskNum();
+            textBoxTask.Text = currTestPass.Test.Tasks[currTestPass.LastTask].Text;
+            listBoxAnswers.Items.Clear();
+            for (int i = 0; i < currTestPass.Test.Tasks[currTestPass.LastTask].Answers.Count; i++)
+            {
+                /*listBoxAnswers.Items.Add(new RadioButton() 
+                                        {
+                                            Text = currTestPass.Test.Tasks[currTestPass.LastTask].Answers[i], 
+                                            Parent = listBoxAnswers, 
+                                            Height = 30,
+                                            Name = "r"+i.ToString(),
+                                        });*/
+                listBoxAnswers.Items.Add(currTestPass.Test.Tasks[currTestPass.LastTask].Answers[i]);
+            }
+            listBoxAnswers.SelectedIndex = currTestPass.Answers[currTestPass.LastTask];
+            listBoxAnswers.SelectedIndex = currTestPass.Test.Tasks[currTestPass.LastTask].RightAnswer;
+        }
+
+        private void buttonEnd_Click(object sender, EventArgs e)
+        {
+            FixTime();
+            currTestPass.TimeEnd = DateTime.Now;
+            client.SendTestResult(currTestPass);
+            var f = new ResultsForm(currTestPass);
+            f.ShowDialog();
+            currTestPass = null;
+
+            panel2.Hide();
+            panel1.Enabled = true;
+        }
+
+        private void buttoNext_Click(object sender, EventArgs e)
+        {
+            if (currTestPass.LastTask < currTestPass.Test.Tasks.Count - 1)
+            {
+                FixTime();
+                currTestPass.LastTask++;
+                ShowTask();
+            }
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            if (currTestPass.LastTask > 0)
+            {
+                FixTime();
+                currTestPass.LastTask--;
+                ShowTask();
+            }
+        }
+
+        private void FixTime()
+        {
+            currTestPass.TimeAnswer[currTestPass.LastTask] += GetTimeResult();
+        }
+
+        private int GetTimeResult()
+        {
+            int res = (DateTime.Now - taskBeginTime).Seconds;
+            taskBeginTime = DateTime.Now;
+            return res;
+        }
+
+        private void listBoxAnswers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currTestPass.Answers[currTestPass.LastTask] = listBoxAnswers.SelectedIndex;
+        }
+
+        private void SetLabelTaskNum()
+        {
+            labelTaskNum.Text = string.Format("Вопрос {0} из {1}", currTestPass.LastTask + 1, currTestPass.Test.Tasks.Count);
+        }
+
+        private void buttonBeginTest_Click(object sender, EventArgs e)
+        {
+            panel2.Show();
+            panel1.Enabled = false;
+            var currTest = tests[listBoxThemes.SelectedIndex].Tests[listBoxTests.SelectedIndex];
+            currTestPass = new TestPassage()
+            {
+                Answers = new List<int>(),
+                Test = currTest,
+                User = currUser,
+                TimeBegin = DateTime.Now,
+                TimeAnswer = new List<int>()
+            };
+            for (int i = 0; i < currTest.Tasks.Count; i++)
+            {
+                currTestPass.TimeAnswer.Add(0);
+                currTestPass.Answers.Add(-1);
+            }
+            ShowTask();  
+        }
+    } 
+    
+}
